8586ceacf7df4b60b39719c626aca498e23464e7
KarateJB <ppippenkimo@gmail.com>
2018-10-29 20:31:04 +0800

    Complete day 15.

--- Vue.js/15. iView-Carousel/README.md
+++ Vue.js/15. iView-Carousel/README.md
@@ -10,3 +10,77 @@
 
 ## 範例
 
+設定`autoplay`後可自動從指定的該筆(`value`值)開始以`autoplay-speed`指定的毫秒輪播。
+`loop`表示開啟循環。
+
+### HTML
+
+```html
+<carousel autoplay loop autoplay-speed="2000" v-model="current">
+    <carousel-item v-for="item in starwars">
+        <div class="card" >
+            <div class="card-body"><h3>{{ item.name }}</h3></div>
+            <img class="card-img-bottom" :src="item.img" style="width:100%;height:100%">
+        </div>
+    </carousel-item>
+</carousel>
+```
+
+### JS
+
+```javascript
+
+const FOO_DATA = [
+    {name:'Luke Skywalker',gender:'male', img:'https://goo.gl/KEUxHN'},
+    //...
+];
+
+
+var app = new Vue({
+    el: "#app",
+    data: {
+        current: 0,
+        starwars:[]
+    },
+    methods: {
+    },
+    created() {
+        this.starwars = FOO_DATA;
+    }
+})
+```
+
+### Demo
+
+![](assets/demo1.gif)
+
+
+
+### 其他設置
+
+| Prop | 描述 | 型態 | 是否必要 | 預設值  |
+|------------|---------------------------------------------------------|---------|------|----------|
+|  dots     | 指示器的位置，`inside`：内部，`outside`：外部，`none`：隱藏 | String  |      | "inside"  |
+|  trigger	| 指示器的觸發方式，可為`click`或`hover`	                 | String  |      | "click"  |
+|  arrow    | 顯示切換箭頭的方式，可為`hover`，`always`，`never`          | String  |      | "hover"  |
+
+
+例如以下HTML：
+
+```html
+<carousel loop dots="outside" trigger="hover" arrow="always" easing="ease" v-model="current" >
+    <carousel-item v-for="item in starwars">
+        <div class="card" >
+            <div class="card-body"><h3>{{ item.name }}</h3></div>
+            <img class="card-img-bottom" :src="item.img" style="width:100%;height:100%">
+        </div>
+    </carousel-item>
+</carousel>
+```
+
+結果如下：
+
+![](assets/demo2.gif)
+
+
+其他API可參考：[Props](https://www.iviewui.com/components/carousel#Carousel_props)及[Events](https://www.iviewui.com/components/carousel#Carousel_events)
--- Vue.js/15. iView-Carousel/sample code/demo.js
+++ Vue.js/15. iView-Carousel/sample code/demo.js
@@ -1,38 +1,29 @@
 
+const FOO_DATA = [
+    {name:'Luke Skywalker',gender:'male', img:'https://goo.gl/KEUxHN'},
+    {name:'Leia Skywalker',gender:'female',img:'https://goo.gl/rNJhLU'},
+    {name:'Anakin Skywalker',gender:'male',img:'https://goo.gl/rvcqJN'},
+    {name:'Padme (Amidala)',gender:'female',img:'https://goo.gl/CNr4WK'},
+    {name:'Rey',gender:'female',img:'https://goo.gl/NEfjfi'},
+    {name:'Obi Wan Kenobi',gender:'male',img:'https://goo.gl/7c5NkR'},
+    {name:'Mace Windu',gender:'male',img:'https://goo.gl/VZsqrH'},
+    {name:'Yoda',gender:'male',img:'https://goo.gl/uJQRGX'},
+    {name:'Darth Vader',gender:'male',img:'https://goo.gl/xcMHqj'},
+    {name:'Darth Sidious',gender:'male',img:'https://goo.gl/QJiJWx'},
+    {name:'Count Dooku',gender:'male',img:'https://goo.gl/sm76q7'},
+    {name:'Darth Maul',gender:'male',img:'https://goo.gl/ikbM7n'}
+];
+
+
 var app = new Vue({
     el: "#app",
     data: {
-        me: {
-            name: "",
-            birthday: "",
-            age: null,
-            description: ""
-        },
-        value: "",
-        msg: "",
-        myUrl: null,
-        myUrlPre: null,
-        myUrlSuf: null
+        current: 2,
+        starwars:[]
     },
     methods: {
-        search() {
-            this.msg = `Searching ${this.value}`;
-        },
-        giveAge(birthday) {
-            if (birthday)
-                this.me.age = this.calAge(birthday);
-        },
-        calAge(targetDate) {
-            var today = new Date();
-            var birthDate = new Date(targetDate);
-            var age = today.getFullYear() - birthDate.getFullYear();
-            var m = today.getMonth() - birthDate.getMonth();
-            if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
-                age--;
-            }
-            return age;
-        }
     },
     created() {
+        this.starwars = FOO_DATA;
     }
 })
