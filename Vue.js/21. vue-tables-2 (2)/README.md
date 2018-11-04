e667289b1642d2141654a53cc303a5c19f1ad5dc
KarateJB <ppippenkimo@gmail.com>
2018-11-04 09:36:02 +0800

    Create app

--- Vue.js/21. vue-tables-2 (2)/README.md
+++ Vue.js/21. vue-tables-2 (2)/README.md
@@ -1,1 +1,11 @@
-nul
+# vue-tables-2 (2)
+
+> 表格套件，本篇主要介紹使用Client及Server table的範本(Template)
+
+## Github
+
+[matfish2/vue-tables-2](https://github.com/matfish2/vue-tables-2)
+
+
+## 範例
+
---
+++ Vue.js/21. vue-tables-2 (2)/sample code/app/src/main.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import App from './App.vue'
+import {ServerTable, ClientTable, Event} from 'vue-tables-2';
+
+//vue-tables-2
+window.axios = require('axios');
+Vue.use(ServerTable, {}, false, 'bootstrap3', 'default');
+Vue.use(ClientTable, {}, false, 'bootstrap3', 'default');
+
+
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
