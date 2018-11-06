2935bb314656ba37823025221d9c5479a45255eb
KarateJB <ppippenkimo@gmail.com>
2018-11-06 12:50:58 +0800

    Refactoring

--- Vue.js/20. vue-tables-2 (1)/sample code/app/src/App.vue
+++ Vue.js/20. vue-tables-2 (1)/sample code/app/src/App.vue
@@ -1,67 +1,25 @@
 <template>
   <div id="app">
-    <div class="row">
-        <div class="col-md-1">
-        </div>
-        <div class="col-md-10">
-             <p>
-              <router-link to="/client-table-demo">Client Table Demo</router-link> / 
-              <router-link to="/server-table-demo">Server Table Demo</router-link>
-            </p>
-            <!-- route outlet -->
-            <router-view></router-view>
-        </div>
-        <div class="col-md-1">
-        </div>
-    </div>
+        <p>
+        <router-link to="/client-table-demo">Client Table Demo</router-link> / 
+        <router-link to="/server-table-demo">Server Table Demo</router-link>
+      </p>
+      <router-view></router-view>
   </div>
 </template>
 
 <script>
-const FOO_DATA = [
-    {id: 1, name:'Luke Skywalker',gender:'male', img:'https://goo.gl/KEUxHN'},
-    {id: 2,name:'Leia Skywalker',gender:'female',img:'https://goo.gl/rNJhLU'},
-    {id: 3,name:'Anakin Skywalker',gender:'male',img:'https://goo.gl/rvcqJN'},
-    {id: 4,name:'Padme (Amidala)',gender:'female',img:'https://goo.gl/CNr4WK'},
-    {id: 5,name:'Rey',gender:'female',img:'https://goo.gl/NEfjfi'},
-    {id: 6,name:'Obi Wan Kenobi',gender:'male',img:'https://goo.gl/7c5NkR'},
-    {id: 7,name:'Mace Windu',gender:'male',img:'https://goo.gl/VZsqrH'},
-    {id: 8,name:'Yoda',gender:'male',img:'https://goo.gl/uJQRGX'},
-    {id: 9,name:'Darth Vader',gender:'male',img:'https://goo.gl/xcMHqj'},
-    {id: 10,name:'Darth Sidious',gender:'male',img:'https://goo.gl/QJiJWx'},
-    {id: 11,name:'Count Dooku',gender:'male',img:'https://goo.gl/sm76q7'},
-    {id: 12,name:'Darth Maul',gender:'male',img:'https://goo.gl/ikbM7n'}
-];
-
 export default {
   name: "app",
   data() {
     return {
-      columns: ["id", "name", "gender"],
-      tableData: [],
-      options: {
-        // filterByColumn:true,
-        // filterable: ['name', 'gender'],
-        requestFunction: function (params) {
-            return axios.get("http://localhost:3000/starwars", {
-              params: params
-            });
-        }
-      }
+      
     };
   },
   methods: {
-    showFilteredCurrentPageData() {
-      //Get the filtered table data on current page
-      console.log(this.$refs.myTable.filteredData);
-    },
-    showFilteredData() {
-      //Get the filtered table data on all pages
-      console.log(this.$refs.myTable.allFilteredData);
-    }
+    
   },
   created() {
-    this.tableData = FOO_DATA;
   }
 };
 </script>
