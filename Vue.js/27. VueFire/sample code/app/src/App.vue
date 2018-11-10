03d58a9737c40e4ba2703fc78bd1e2146cd77a10
KarateJB <ppippenkimo@gmail.com>
2018-11-10 17:39:25 +0800

    Create sample

---
+++ Vue.js/27. VueFire/sample code/app/src/App.vue
@@ -0,0 +1,112 @@
+<template>
+   <div id="app">
+      <div class="menuBtn" @click="toggleMenu">
+        <!-- <i class="blue fa fa-th-list fa-2x"></i> -->
+        <button class="btn btn-default">Menu</button>
+      </div>
+      <div class="topMenu" v-show="menuState==='in'">
+        <ul>
+          <li>
+            <router-link to="/prod-list"><i class="fa fa-file-powerpoint-o"></i>Buy something</router-link>
+          </li>
+          <li>
+            <router-link to="/orders"><i class="fa fa-file-powerpoint-o"></i>My order</router-link>
+          </li>
+          <li v-if="!isAuth">
+            <router-link to="/login" tag="button" class="btn btn-success">            
+              <!-- <i class="fa fa-google-plus"></i> -->
+              Login
+            </router-link>
+          </li>
+          <li v-if="isAuth===true">
+            <button class="btn btn-danger" @click="logout()">
+              <!-- <i class="fa fa-google-plus"></i> -->
+              <any v-if="isAuth">Logout as {{user.displayName}}</any>
+            </button>
+          </li>
+        </ul>
+      </div>
+  
+     <!-- route outlet -->
+      <router-view></router-view>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      menuState: "out",
+      user: null,
+      isAuth: false
+    }
+  },
+  methods:{
+    toggleMenu(){
+          this.menuState = (this.menuState === 'out' ? 'in' : 'out');
+    },
+    logout() {
+      firebaseAuth.signOut()
+        .then(() => {
+          this.user = null;
+          this.$router.replace("/login");
+        }).catch(err => console.log(error));
+    }
+  },
+  mounted(){
+    this.user = firebaseAuth.currentUser;
+    this.isAuth = this.user ? true : false;
+  }
+}
+</script>
+
+<style>
+a {
+    cursor: pointer;
+}
+
+.topMenu {
+  background:#000;
+  color: #fff;
+  position: fixed;
+  left: auto;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  width: 20%;
+  min-width: 250px;
+  z-index: 9999;
+  font-family: Consolas;
+}
+
+.menuBtn {
+  z-index: 9999;
+  position: absolute;
+  opacity: 0.6;
+}
+
+.menuBtnFake {
+  z-index: 9999;
+  position: absolute;
+}
+
+ul {
+  font-size: 18px;
+  line-height: 3;
+  font-weight: 400;
+  padding-top: 50px;
+  list-style: none;
+}
+
+ .center {
+    text-align: center; /* center checkbox horizontally */
+    vertical-align: middle; /* center checkbox vertically */
+    margin: auto;
+    width: 80%;
+    /* border: 3px solid green; */
+    padding: 10px;
+}
+</style>
---
+++ Vue.js/27. VueFire/sample code/app/src/components/prod-list.vue
@@ -0,0 +1,70 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-1">
+            </div>
+            <div class="col-md-10">
+              <pre>{{ fbObject }}</pre>
+              <table class="table">
+                <thead class="thead-dark">
+                  <tr>
+                    <th>Type</th>
+                    <th>ID</th>
+                    <th>Title</th>
+                    <th>Price</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr v-for="item in fbArray" :key="item.id">
+                    <td>{{ item.type }}</td>
+                    <td>{{ item.id }}</td>
+                    <td>{{ item.title }}</td>
+                    <td>{{ item.price }}</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+            <div class="col-md-1">
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'prod-list',
+  data () {
+    return {
+    }
+  },
+//   firebase: {
+//       fbArray: firebaseDb.ref('Demo').limitToLast(10), //bind as an array
+//       fbObject: {
+//         source: firebaseDb.ref('Demo/products/800afd3c-1615-49ba-b33d-497842af6c82'),
+//         asObject: true, //Bind as object
+//         cancelCallback: function () {
+//           console.log("Not ready for the object!");
+//         },
+//         readyCallback: function () { 
+//           console.log("Get ready for the object!");
+//       }
+//     }
+//   },
+//   mounted(){
+//     // let products = db.ref('/Demo/products/');
+//     console.log(this.fbArray);
+//     console.log(this.fbObject);
+//     console.log(this.demo);
+//     // this.afDb.object<Product>('/Demo/products/' + key).valueChanges().take(1).map(x => <Product>x);
+//   },
+  created(){
+      console.log(firebaseDb);
+      console.log(firebaseAuth);
+  }
+}
+</script>
+
+<style>
+</style>
