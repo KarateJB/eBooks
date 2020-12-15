17cb1a63055d42923eba8127cf324e6b3438f5e5
KarateJB <ppippenkimo@gmail.com>
2020-12-15 17:14:04 +0800

    Refactor

--- DevOps/Docker/06. Dockerfile/samples/postgres_init_and_migrate/postgres/migrate_db.sh
+++ DevOps/Docker/06. Dockerfile/samples/postgres_init_and_migrate/postgres/migrate_db.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-psql -p $POSTGRES_PORT -U $POSTGRES_USER -d $POSTGRES_DB -f /docker-entrypoint-initdb.d/script.sql
+psql -p $POSTGRES_PORT -U $POSTGRES_USER -d $POSTGRES_DB -f /docker-entrypoint-initdb.d/migrate_script.sql
 echo
