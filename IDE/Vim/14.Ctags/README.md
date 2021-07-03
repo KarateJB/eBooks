3af3c2c512cd337718c56a989d92aedf64c17151
KarateJB <ppippenkimo@gmail.com>
2021-07-04 05:40:20 +0800

    Create JS sample

--- IDE/Vim/14.Ctags/README.md
+++ IDE/Vim/14.Ctags/README.md
@@ -2,6 +2,20 @@
 
 ## Install
 
+### Ubuntu
+
+```s
+$ sudo apt-get install exuberant-ctags
+# Or
+# sudo apt-get install ctags
+$ ctags --version
+Exuberant Ctags 5.9~svn20110310, Copyright (C) 1996-2009 Darren Hiebert
+  Addresses: <dhiebert@users.sourceforge.net>, http://ctags.sourceforge.net
+```
+
+
+We can install the maintained impementation of ctags: [Unerversal Ctags](https://github.com/universal-ctags/ctags).
+
 
 ### Windows 
 
@@ -16,4 +30,7 @@ Set the path to environment variable:
 
 ```s
 $ setx path "%PATH%; C:\ProgramData\chocolatey\lib\Emacs\tools\emacs\bin" /m
+$ ctags --version
+ctags (GNU Emacs 27.2)
+Copyright (C) 2021 Free Software Foundation, Inc.
 ```
---
+++ IDE/Vim/14.Ctags/sample/index.js
@@ -0,0 +1,59 @@
+import { isMatch } from "./modules/match_reverse.js";
+
+let reInitSearchIndex = (currentIndex, pattern_len) => {
+  return {
+    start: currentIndex.end + 1,
+    end: currentIndex.end + pattern_len
+  };
+};
+
+var reverseStringSplit = (originArr) => {
+    let counters = [];
+    let answer = 0;
+    // let originArr = s.split();
+    
+    let index = 0;
+    let i = 0;
+    
+    while( i < originArr.length){
+         let counter = 0;
+         let currentIndex = {
+           start: i,
+           end: i
+         };
+         let searchIndex = reInitSearchIndex(currentIndex, 1);
+      
+         while( currentIndex.end < originArr.length ){
+           
+           let pattern = originArr.substr(currentIndex.start, currentIndex.end - currentIndex.start + 1);
+           let patternToCompare = originArr.substr(searchIndex.start, searchIndex.end - searchIndex.start + 1);
+           
+           var isMatchFlag = isMatch(pattern, patternToCompare)
+           console.log('pattern','patternToCompare', pattern, patternToCompare, isMatchFlag);
+           
+           if( isMatchFlag ){
+             counter++;
+             currentIndex = {
+               start: searchIndex.end + 1,
+               end: searchIndex.end + 1
+             };
+           } else if(searchIndex.end < originArr.length){
+             currentIndex.end +=1;
+           } else {
+             currentIndex.start ++;
+             currentIndex.end = currentIndex.start;
+           }
+   
+           searchIndex = reInitSearchIndex(currentIndex, currentIndex.end - currentIndex.start + 1);
+         }
+      
+         console.log(`---- Match number = ${counter} ----`);
+         counters.push(counter);
+         i++;
+    }
+    
+    console.log('counters', counters);
+    return Math.max(...counters);
+};
+
+console.log(reverseStringSplit("LRLRLR"));
