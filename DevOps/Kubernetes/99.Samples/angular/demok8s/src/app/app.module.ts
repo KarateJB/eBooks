0b36dee7ca1b533914a65a83994b8f3fb820790c
KarateJB <ppippenkimo@gmail.com>
2021-07-16 15:12:32 +0800

    Add hello world component

--- DevOps/Kubernetes/99.Samples/angular/demok8s/src/app/app.module.ts
+++ DevOps/Kubernetes/99.Samples/angular/demok8s/src/app/app.module.ts
@@ -1,15 +1,18 @@
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HelloWorldComponent } from './component/hello-world/hello-world.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloWorldComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule,
     AppRoutingModule
   ],
   providers: [],
